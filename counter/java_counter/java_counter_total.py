c694fc634b0f6122dd24dec0044d39eb481cb1b4
patronus49 <34188304+patronus49@users.noreply.github.com>
2025-04-14 16:37:41 +0530

    Add type to count var

--- counter/java_counter/java_counter_total.py
+++ counter/java_counter/java_counter_total.py
@@ -25,7 +25,7 @@ class JavaCounterTotal(SyntaxCounterOperations):
 
     def count(self) -> None:
         """ counts the total lines code type in input """
-        count = 0
+        count: int = 0
 
         with open(self.__get_path__(), 'r') as file:
             for line in file:
